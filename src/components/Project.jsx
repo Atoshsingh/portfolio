204a4dc0cf9a8b42049e74b07a7cd5b7e8c8a0c1
Atoshsingh <toshkumrt@gmail.com>
2024-09-01 14:41:08 +0530

    second Commit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 const Project=()=>{
 
   return (
-    <div className='absolute md:top-[64px] w-full md:h-[calc(100vh-64px)] top-0 h-screen '>
+    <div className='absolute md:top-[64px] w-full md:h-[calc(100vh-64px)] top-0 h-screen bg-red-600'>
       <div className=''></div>
     </div>
   )
