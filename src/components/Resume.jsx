dbf4846244f0edeeed3828ed6ee32cb6500f763f
Atoshsingh <toshkumrt@gmail.com>
2024-09-06 17:43:25 +0530

    updated resume page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,8 +6,8 @@ const Resume = () => {
     <div className=' md:top-[64px] w-full md:h-[calc(100vh-64px)] top-0 h-screen'>
       <div className='w-full p-20  flex flex-col items-center justify-center '>
       <p className='text-[#77d6ff] text-2xl md:text-4xl hover:scale-110 duration-500 font-extrabold'>RESUME</p>
-        <div className='h-[600px] md:h-[1300px]  flex flex-row justify-center items-center'>
-          <img className='xl:w-[860px] hover:scale-95 xl:h-[1200px] md:w-[640px] md:h-[1010px] w-[300px] h-[450px] transition-all ease-in-out duration-700 rounded-2xl' src={AashutoshJpg} alt='my remsume pic' />
+        <div className='h-[600px] w-[300px] md:h-[1300px]  flex flex-row justify-center items-center'>
+          <img className='xl:w-[860px] hover:scale-95 xl:h-[1200px] md:w-[640px] md:h-[1010px] w-[400px] h-[420px] transition-all ease-in-out duration-700 rounded-2xl' src={AashutoshJpg} alt='my remsume pic' />
         </div>
         <div className='h-[50px]  flex justify-center items-center'>
           <a className='curser-pointer font-mochiy hover:scale-95 bg-[#54c8f9] hover:bg-[#1184b6] px-10 py-3 rounded-md duration-500 hover:text-white ' href={Aashutosh} download="Aashutosh Singh (Resume) " type="application/pdf">
