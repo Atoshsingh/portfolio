ce0317afd2ab4171a2016eeacb7e5451bf4bbef8
Atoshsingh <toshkumrt@gmail.com>
2024-09-06 17:49:35 +0530

    updated resume page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,7 +6,7 @@ const Resume = () => {
     <div className=' md:top-[64px] w-full md:h-[calc(100vh-64px)] top-0 h-screen'>
       <div className='w-full p-20  flex flex-col items-center justify-center '>
       <p className='text-[#77d6ff] text-2xl md:text-4xl hover:scale-110 duration-500 font-extrabold'>RESUME</p>
-        <div className='h-[600px] w-[300px] md:h-[1300px]  flex flex-row justify-center items-center'>
+        <div className='h-[500px] w-[300px] md:w-[1200px] md:h-[1300px]  flex flex-row justify-center items-center '>
           <img className='xl:w-[860px] hover:scale-95 xl:h-[1200px] md:w-[640px] md:h-[1010px] w-[400px] h-[420px] transition-all ease-in-out duration-700 rounded-2xl' src={AashutoshJpg} alt='my remsume pic' />
         </div>
         <div className='h-[50px]  flex justify-center items-center'>
